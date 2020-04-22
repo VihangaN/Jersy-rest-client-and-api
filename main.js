346e1fa4f33307e15145f9185c680c4d0e54210b
vihanga nivarthana <vihanganivarthana@vihangas-MacBook-Air.local>
2020-04-22 17:22:04 +0530

    UI updated

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $('document').ready(() => {
 
   
 
-// toggle button
+// toggle button events
 
     const btn = $("#staffType");
     const typetext = $("#type");
@@ -30,6 +30,24 @@ $('document').ready(() => {
     
     
     })
+
+
+    // tooltip
+
+
+    $("#viewAll").mouseenter(()=> {
+      $("#tooltip").animate({width: '150px'}).animate({height: 'auto'}).animate({opacity: '1'},(10));
+  }).mouseleave(()=> {
+       $("#tooltip").animate({width: '0px'}).animate({opacity: '0'},(1));
+  });
+
+
+  // View All button events
+
+
+  $("#viewAll").click(()=>{
+    allMedical();
+  })
 
 // get medical staff
 
@@ -72,8 +90,42 @@ nonMedical= ()=>{
   
 }
 
+// getAll data
+
+
+allMedical=()=>{
+  typetext.text(" All ");
+  
+  $.ajax({ 
+    type: 'GET', 
+    url: 'http://dummy.restapiexample.com/api/v1/employees', 
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: (data)=> { 
+      console.log(data);
+        $.each(data.data, function(key, value) {
+          $("tbody").append(`
+          <tr></tr>
+          <tr>
+                  <td>${value.id}</td>
+                  <td>${value.employee_name}</td>
+                  <td>${value.employee_age}</td>
+                  <td>${value.employee_salary}</td>
+                  <td>${value.profile_image}</td>
+                  <td>
+                  <button id="edit"></button>
+                  <button id="delete"></button>
+                  </td>
+                </tr>
+          
+          `);
+        });
+    }
+  });
+}
 
 // initialize data
+
 
 typetext.text(" All ");
 

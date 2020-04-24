3188074320f36ae057633427028111557961edb3
vihanga nivarthana <vihanganivarthana@vihangas-MacBook-Air.local>
2020-04-24 22:16:24 +0530

    new font and progress bar added

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ $('document').ready(() => {
 
     const btn = $("#staffType");
     const typetext = $("#type");
-   
-   
+    const modal = $("#modal");
+    let memberCount = 0;
+    let med = 0;
+    let non =0;
+    
 
     $(btn).click(()=>{
 
@@ -36,7 +39,7 @@ $('document').ready(() => {
 
 
     $("#viewAll").mouseenter(()=> {
-      $("#tooltip").animate({width: '150px'}).animate({height: 'auto'}).animate({opacity: '1'},(10));
+      $("#tooltip").animate({width: '180px'}).animate({height: 'auto'}).animate({opacity: '1'},(10));
   }).mouseleave(()=> {
        $("#tooltip").animate({width: '0px'}).animate({opacity: '0'},(1));
   });
@@ -60,11 +63,13 @@ medical= ()=>{
     dataType: 'json',
     success: (data)=> { 
       console.log(data);
+      $('#data').find('tbody').empty();
         $.each(data.data, function(key, value) {
+
           $("tbody").append(`
           <tr></tr>
           <tr>
-                  <td>${value.id}</td>
+                  <td id="id">${value.id}</td>
                   <td>${value.employee_name}</td>
                   <td>${value.employee_age}</td>
                   <td>${value.employee_salary}</td>
@@ -76,7 +81,13 @@ medical= ()=>{
                 </tr>
           
           `);
+
+          med=med+1;
         });
+
+        progress("med",18,memberCount);
+        pcount(18);
+        med=0;
     }
   });
 
@@ -88,14 +99,6 @@ medical= ()=>{
 
 nonMedical= ()=>{
   
-}
-
-// getAll data
-
-
-allMedical=()=>{
-  typetext.text(" All ");
-  
   $.ajax({ 
     type: 'GET', 
     url: 'http://dummy.restapiexample.com/api/v1/employees', 
@@ -103,11 +106,65 @@ allMedical=()=>{
     dataType: 'json',
     success: (data)=> { 
       console.log(data);
+      $('#data').find('tbody').empty();
         $.each(data.data, function(key, value) {
-          $("tbody").append(`
+
+          if(value.employee_age > 30){
+
+            $("tbody").append(`
+            <tr></tr>
+            <tr>
+                    <td id="id">${value.id}</td>
+                    <td>${value.employee_name}</td>
+                    <td>${value.employee_age}</td>
+                    <td>${value.employee_salary}</td>
+                    <td>${value.profile_image}</td>
+                    <td>
+                    <button id="edit"></button>
+                    <button id="delete"></button>
+                    </td>
+                  </tr>
+            
+            `);
+
+            non= non+1;
+            pcount(6);
+          }
+
+          else{
+
+          }
+         
+        });
+
+        progress("non",6,memberCount);
+        non =0;
+    }
+  });
+
+
+
+}
+
+// getAll data
+
+
+allMedical=()=>{
+  typetext.text(" All ");
+  $('#data').find('tbody').empty();
+  $.ajax({ 
+    type: 'GET', 
+    url: 'http://dummy.restapiexample.com/api/v1/employees', 
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: (data)=> { 
+      console.log(data);
+      
+        $.each(data.data, function(key, value) {
+          $('#data').find('tbody').append(`
           <tr></tr>
           <tr>
-                  <td>${value.id}</td>
+                  <td id="id">${value.id}</td>
                   <td>${value.employee_name}</td>
                   <td>${value.employee_age}</td>
                   <td>${value.employee_salary}</td>
@@ -119,12 +176,18 @@ allMedical=()=>{
                 </tr>
           
           `);
+
+          
         });
+
+        progress(memberCount);
+        pcount(memberCount);
     }
   });
 }
 
 // initialize data
+
 
 
 typetext.text(" All ");
@@ -139,25 +202,29 @@ $.ajax({
       $.each(data.data, function(key, value) {
 
        
-        $("tbody").append(`
-        <tr></tr>
-        <tr>
-                <td>${value.id}</td>
-                <td>${value.employee_name}</td>
-                <td>${value.employee_age}</td>
-                <td>${value.employee_salary}</td>
-                <td>${value.profile_image}</td>
-                <td>
-                <button id="edit"></button>
-                <button id="delete"></button>
-                </td>
-              </tr>
-        
-        `);
+        $('#data').find('tbody').append(`
+          <tr></tr>
+          <tr>
+                  <td id="id">${value.id}</td>
+                  <td>${value.employee_name}</td>
+                  <td>${value.employee_age}</td>
+                  <td>${value.employee_salary}</td>
+                  <td>${value.profile_image}</td>
+                  <td>
+                  <button id="edit"></button>
+                  <button id="delete"></button>
+                  </td>
+                </tr>
+          
+          `);
     
-
-     
+          memberCount = data.data.length;
+          console.log(memberCount);
+         
       });
+
+      pcount(memberCount);
+     
   }
 });
 
@@ -198,8 +265,93 @@ $('#search').keyup(function(){
   
 })
 
+//  event listners for modal
 
 
+
+
+$("#data").on('click','#edit',(function() {
+  const id = $(this).closest("tr").find("#id").text();   
+  popup('edit',id);
+}));
+
+ $("#addMember").click(()=>{
+  popup('add');
+ })
+
+
+// modal
+
+let modalTitle = $("#mtitle");
+
+popup = (type,id)=>{
+  if(type=='edit'){
+    modalTitle.text(`Edit member ${id}`);
+    modal.fadeIn();
+  }
+  else if(type=='add'){
+    modalTitle.text("Add new member");
+    modal.fadeIn();
+  }
+  
+ 
+}
+
+
+// Radial progress
+
+progress = (type,part,all)=>{
+ 
+
+  let present;
+
+  if(part == 0){
+    present = 1;
+    $("#progress").animate({'stroke-dashoffset': progress}).css({'stroke':'#F4F4F4','transition':'1s'});
+  
+  }
+  else{
+  
+  present = (part/all)*100/100;
+  console.log("100 present "+present);
+  
+  let progress = 339.292 * (1 - present);
+  console.log(`presentage is ${progress}`);
+  
+  let color = "#e6e6e6";
+  
+  let nonColor = "#ff9f1a";
+  let medColor ="#3498DB";
+
+  
+  if(type=="med"){
+    color =  medColor;
+  }
+  else if(type== "non"){
+    color = nonColor;
+  }
+
+  else{
+    color="#e6e6e6"
+  }
+  
+  $("#progress").animate({'stroke-dashoffset': progress}).css({'stroke':color,'transition':'1s'});
+  
+  
+  
+  }
+
+  
+}
+
+// update progress number
+
+
+pcount = (num)=>{
+  $("#progress_count").text(num);
+}
+  
+  
 
 });
 
